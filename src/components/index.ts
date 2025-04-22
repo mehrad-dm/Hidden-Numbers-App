de139b135298bdb5c001396a61e325417c52f3a7
mehrad-dm <mehrad.deldadeh@gmail.com>
2025-04-22 04:11:23 +0330

    feat(game result): Implement the game result component.

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,4 +1,7 @@
 export * from "./card";
+export * from "./card-list";
+export * from "./status-panel";
 export * from "./input-row";
 export * from "./feedback-icons";
 export * from "./game-board";
+export * from "./game-result";
